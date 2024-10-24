1b2508fbb71f51c9ae88202a26e2b41d8955c1d2
Felipe Peruceli <105284817+SoyBrother@users.noreply.github.com>
2024-10-24 08:46:44 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -7,7 +7,7 @@ async function visualizarInformacoesGlobais(){
     cosole.log(dados);
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_com_acesso_a_educacao} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio_dia_estudando} horas conectadas.`
+    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_com_acesso_a_educacao} têm acesso a educação e passam ${dados.tempo_medio_dia_estudando} horas estudando e ${dados.total_pessoas_com_educacao_superior}tiveram acesso a educação superior.`
   }
 
 visualizarInformacoesGlobais();

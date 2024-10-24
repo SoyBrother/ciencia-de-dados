598a4149746d869472dd93a207a5c0de1caf0dc3
Felipe Peruceli <105284817+SoyBrother@users.noreply.github.com>
2024-10-24 08:48:32 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -2,9 +2,10 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 
 async function visualizarInformacoesGlobais(){
     const res = await fetch(url)
+    const dados = res.json()
 
-    const dados = await res.json()
-    cosole.log(dados);
+
+    
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
     paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_com_acesso_a_educacao} têm acesso a educação e passam ${dados.tempo_medio_dia_estudando} horas estudando e ${dados.total_pessoas_com_educacao_superior}tiveram acesso a educação superior.`
